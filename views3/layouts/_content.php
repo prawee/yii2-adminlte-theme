88fb15086b85acb3dfdfcd9e3ee7ea24ff56f1c0
Prawee Wongsa <prawee.wongsa@allianz.com>
2020-03-18 13:57:43 +0700

    update content layout with breadcrumbs

--- views3/layouts/_content.php
+++ views3/layouts/_content.php
@@ -1,16 +1,33 @@
 <?php
 /**
- * Description of _footer.php
+ * Description of _content.php
  * Created At 14/02/2020 2:00 PM
  * Author By Prawee Wongsa <konkeanweb@gmail.com>
  */
+
+use yii\bootstrap4\Breadcrumbs;
 ?>
-<div class="content-wrapper px-4 py-2">
+<div class="content-wrapper">
     <div class="content-header">
-        <h1 class="text-dark">Layout</h1>
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 text-dark">
+                        <?=isset($this->title) ? $this->title : null?>
+                    </h1>
+                </div>
+                <div class="col-sm-6">
+                    <?=Breadcrumbs::widget([
+                        'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+                        'tag' => 'ol',
+                        'options' => ['class' => 'breadcrumb float-sm-right']
+                    ])?>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <div class="content px-2">
+    <div class="content">
         <?=$content?>
     </div>
 </div>
