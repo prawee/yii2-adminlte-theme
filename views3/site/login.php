4f2238534fc23c747cc55df9ac1f54313fa54aff
Prawee Wongsa <prawee.wongsa@allianz.com>
2020-04-28 12:59:41 +0700

    added login and signup form for v3

---
+++ views3/site/login.php
@@ -0,0 +1,35 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap4\ActiveForm */
+/* @var $model app\models\LoginForm */
+
+use yii\bootstrap4\Html;
+use yii\bootstrap4\ActiveForm;
+
+$this->title = 'Login';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="card-body login-card-body">
+
+    <p class="login-box-msg">Please fill out the following fields to login:</p>
+
+    <?php $form = ActiveForm::begin([
+        'id' => 'login-form',
+    ]); ?>
+
+        <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+
+        <?= $form->field($model, 'password')->passwordInput() ?>
+
+        <?= $form->field($model, 'rememberMe')->checkbox() ?>
+
+        <div class="form-group">
+            <div class="col-lg-offset-1 col-lg-11">
+                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                <?= Html::tag('a', 'Signup', ['class' => 'btn btn-default', 'href' => '/site/signup'])?>
+            </div>
+        </div>
+
+    <?php ActiveForm::end(); ?>
+</div>
---
+++ views3/site/signup.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @link http://www.konkeanweb.com
+ * 18/03/2020 10:45 AM
+ * @copyright Copyright (c) 2020 served
+ * @author Prawee Wongsa <konkeanweb@gmail.com>
+ * @license BSD-3-Clause
+ */
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap4\ActiveForm */
+/* @var $model \frontend\models\SignupForm */
+
+use yii\helpers\Html;
+use yii\bootstrap4\ActiveForm;
+
+$this->title = 'Signup';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="card-body login-card-body">
+
+    <p>Please fill out the following fields to signup:</p>
+
+    <div class="row">
+        <div class="col-lg-5">
+            <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
+
+            <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+
+            <?= $form->field($model, 'email') ?>
+
+            <?= $form->field($model, 'password')->passwordInput() ?>
+
+            <div class="form-group">
+                <?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
+                <?= Html::tag('a', 'Login', ['class' => 'btn btn-default', 'href' => '/site/login'])?>
+            </div>
+
+            <?php ActiveForm::end(); ?>
+        </div>
+    </div>
+</div>
